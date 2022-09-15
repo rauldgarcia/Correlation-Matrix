c90538da5e6032f8718a79d517d854f658a83380
Raul Garcia <rauld.garcia95@gmail.com>
2022-09-14 21:46:23 -0500

    Codigo final

--- CovarianceMatrix.py
+++ CovarianceMatrix.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import random
 import math
+import pandas as pd
 
 #Transpuesta de matriz
 def transpose(matrix):
@@ -18,28 +19,20 @@ def transpose(matrix):
     return result
 
 #Carga de imagen
-imagen = cv2.imread('imagen3.jpg')
+imagen = cv2.imread('imagen7.jpeg')
 #cv2.imshow('imagen',imagen)
 cv2.waitKey(0)
-resolucionx=3840
-resoluciony=2160
+resolucionx=800
+resoluciony=800
 
 #Vector auxiliar para calcular vector promedio
-sumrgb=np.array([
-    [0],
-    [0],
-    [0]
-])
+sumrgb=np.zeros((3,1))
 
 #Contador auxiliar para calcular promedio
 cont=0
 
 #Creacion de vector promedio
-vm=np.array([
-    [0],
-    [0],
-    [0]
-])
+vm=np.zeros((3,1))
 
 #Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
 for y in range (resoluciony):
@@ -50,8 +43,9 @@ for y in range (resoluciony):
             [g],
             [b]
         ])
+        rgb=rgb/255;
         sumrgb=sumrgb+rgb
-        cont+=1
+        cont=cont+1
 
 #creacion de vector auxiliar para calculo de vector promedio
 aux=np.array([
@@ -66,11 +60,8 @@ print("\nEl vector promedio es:")
 print(vm)
 
 #Creacion de matriz auxiliar
-sumamatriz=np.array([
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-])
+sumamatriz=np.zeros((3,3))
+cont=0
 
 #Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
 for y in range (resoluciony):
@@ -81,6 +72,7 @@ for y in range (resoluciony):
             [g],
             [b]
         ])
+        rgb=rgb/255;
         
         #Calculo de vector x-m
         x_m=rgb-vm
@@ -89,40 +81,18 @@ for y in range (resoluciony):
         #Creacion de matriz (x-m)(x-m)^t
         matriz=x_m*x_mt
         sumamatriz=sumamatriz+matriz
-        cont+=1
-
-#Creacion de matriz auxiliar
-maux=np.array([
-    [cont-1,cont-1,cont-1],
-    [cont-1,cont-1,cont-1],
-    [cont-1,cont-1,cont-1]
-])
+        cont=cont+1
 
 #calculo de matriz de covarianza
-cx=np.array([
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-])
-cx=sumamatriz/maux
+cx=np.zeros((3,3))
+cx=sumamatriz/(cont-1);
 print("\nLa matriz de covarianza es:")
 print(cx)
 
-#Creacion de matriz auxiliar para calculo de matriz de correlacion
-cii=cx[0][0]
-cjj=cx[1][1]
-ckk=cx[2][2]
-denominador=pow(cii*cjj*ckk,0.5)
-matrizaux=np.array([
-    [denominador,denominador,denominador],
-    [denominador,denominador,denominador],
-    [denominador,denominador,denominador]
-])
-
-#Calculo de matriz de correlacion
-rx=cx/maux
-print("\nLa matriz de correlacion es:")
-print(rx)
+df=pd.DataFrame(cx)
+cor=df.corr()
+print('\nLa matriz de correlación es:')
+print(cor)
 
 #Calculo del determinante de la matriz de covarianza
 det=np.linalg.det(cx)
@@ -137,16 +107,15 @@ for i in range (3):
         [g],
         [b]
     ])
+    rgb=rgb/255;
     print("\nEl pixel es: ({},{})".format(pixx,pixy))
     print(rgb)
     x_mu=rgb-vm
     x_mut=transpose(x_mu)
-    auxiliar2=np.array([
-        [0,0,0]
-    ])
+    auxiliar2=np.zeros((1,3))
     cxi=np.linalg.inv(cx)
     auxiliar1=np.matmul(x_mut,cxi)
     auxiliar2=np.matmul(auxiliar1,x_mu)
     n=(1/((2*3.1416)**1.5))*(1/((det)**0.5))*(math.exp(-0.5*auxiliar2))
-    print("La probabilidad de cercania al color promedio es:")
-    print(n*100)
+    print("La cercania al color promedio es:")
+    print(n)
