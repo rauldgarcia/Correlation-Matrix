e60f04056d1bc9b09fe9f78f0c6441c1679a74fd
Raul Garcia <rauld.garcia95@gmail.com>
2022-09-16 20:17:52 -0500

    Ploteo de pixeles en 3D y calculo de eigenvectores

--- CovarianceMatrix.py
+++ CovarianceMatrix.py
@@ -6,6 +6,10 @@ import cv2
 import random
 import math
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import time
+inicio=time.time()
 
 #Transpuesta de matriz
 def transpose(matrix):
@@ -24,15 +28,26 @@ imagen = cv2.imread('imagen7.jpeg')
 cv2.waitKey(0)
 resolucionx=800
 resoluciony=800
+print("La resolución de la imagen es:")
+print(resolucionx)
+print(resoluciony)
+print("\nEl total de pixeles es:")
+print(resolucionx*resoluciony)
 
 #Vector auxiliar para calcular vector promedio
 sumrgb=np.zeros((3,1))
 
 #Contador auxiliar para calcular promedio
 cont=0
+contador=0
 
 #Creacion de vector promedio
 vm=np.zeros((3,1))
+
+x1=np.zeros((1,1))
+y1=np.zeros((1,1))
+z1=np.zeros((1,1))
+color=np.zeros((1,3))
 
 #Extraer componentes B,G,R de un solo pixel (opencv los da en ese orden y se cargan las coordenadas en orden Y X)
 for y in range (resoluciony):
@@ -43,19 +58,14 @@ for y in range (resoluciony):
             [g],
             [b]
         ])
+        crgb=np.array([[r/255,g/255,b/255]]) #array que guarda color de pixel actual
         rgb=rgb/255;
+        color=np.append(color,crgb,axis=0)   #array que guarda color de cada pixel
         sumrgb=sumrgb+rgb
         cont=cont+1
 
-#creacion de vector auxiliar para calculo de vector promedio
-aux=np.array([
-    [cont],
-    [cont],
-    [cont]
-])
-
 #Calculo de vector promedio
-vm=sumrgb/aux
+vm=sumrgb/cont;
 print("\nEl vector promedio es:")
 print(vm)
 
@@ -83,7 +93,6 @@ for y in range (resoluciony):
         sumamatriz=sumamatriz+matriz
         cont=cont+1
 
-#calculo de matriz de covarianza
 cx=np.zeros((3,3))
 cx=sumamatriz/(cont-1);
 print("\nLa matriz de covarianza es:")
@@ -98,7 +107,7 @@ print(cor)
 det=np.linalg.det(cx)
 
 #Extraccion de 3 pixeles al azar
-for i in range (3):
+for i in range (1):
     pixx=random.randint(0,resolucionx)
     pixy=random.randint(0,resoluciony)
     (b, g, r) = imagen[pixy,pixx]
@@ -119,3 +128,53 @@ for i in range (3):
     n=(1/((2*3.1416)**1.5))*(1/((det)**0.5))*(math.exp(-0.5*auxiliar2))
     print("La cercania al color promedio es:")
     print(n)
+
+#calculo de eigenvalores y eigenvectores 
+eigenvalor, eigenvector=np.linalg.eig(cx)
+print("\nLos eigenvalores son:")
+print(eigenvalor)
+print("\nLos eigenvectores son:")
+print(eigenvector)
+
+#creacion de vector medio
+vmx=(0,vm[0])
+vmy=(0,vm[1])
+vmz=(0,vm[2])
+
+#creacion de eigenvector 1
+ev1x=(vm[0],eigenvector[0][0])
+ev1y=(vm[1],eigenvector[0][1])
+ev1z=(vm[2],eigenvector[0][2])
+
+#creacion de eigenvector 2
+ev2x=(vm[0],eigenvector[1][0])
+ev2y=(vm[1],eigenvector[1][1])
+ev2z=(vm[2],eigenvector[1][2])
+
+#creacion de eigenvector 3
+ev3x=(vm[0],eigenvector[2][0])
+ev3y=(vm[1],eigenvector[2][1])
+ev3z=(vm[2],eigenvector[2][2])
+
+#creacion de los array de xyz
+colores=pd.DataFrame(color)
+x1=colores[0]
+y1=colores[1]
+z1=colores[2]
+
+#Grafico
+fig=plt.figure()
+axes=plt.axes(projection='3d')
+axes.scatter3D(x1,y1,z1,c=color)
+axes.plot(vmx,vmy,vmz)
+axes.plot(ev1x,ev1y,ev1z)
+axes.plot(ev2x,ev2y,ev2z)
+axes.plot(ev3x,ev3y,ev3z)
+axes.set_title("Grafico de distribución de pixeles")
+axes.set_xlabel("R")
+axes.set_ylabel("G")
+axes.set_zlabel("B")
+fin=time.time()
+print("El tiempo de ejecución es:")
+print(fin-inicio)
+plt.show()
